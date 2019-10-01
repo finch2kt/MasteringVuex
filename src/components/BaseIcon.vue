7132c78a24334ff764ca9a31586f4ce655cfddd9
Kashaun Finch <kashaunt.finch@gmail.com>
2019-10-01 18:47:10 -0400

    Init Mastering vuex

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :hieght="hieght">
+      <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }" />
+    </svg>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    hieght: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/router.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import EventList from './views/EventList.vue';
+import EventShow from './views/EventShow.vue';
+import EventCreate from './views/EventCreate.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventList
+    },
+    {
+      path: '/event/:id',
+      name: 'event-show',
+      component: EventShow,
+      props: true
+    },
+    {
+      path: '/event/create',
+      name: 'event-create',
+      component: EventCreate
+    }
+
+    // {
+    //   path: '/about-us',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ './views/About.vue')
+    //   // Alias
+    //   // alias: '/about'
+    // }
+    // {
+    //   // Redirect
+    //   path: '/about',
+    //   redirect: { name: 'about' }
+    // }
+  ]
+});
